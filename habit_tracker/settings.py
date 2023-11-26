52566aaaba7957e5b1fd8d3412064013b9931061
guru-raghavendra <hguru001@gmail.com>
2023-11-26 15:14:58 +0530

    adding vercel

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$nw)f($%801qk4)_cmh&r0jf-+rke7_(aq!l4ji%dzb_b8$xem'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
--- habit_tracker/wsgi.py
+++ habit_tracker/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habit_tracker.settings')
 
 application = get_wsgi_application()
+
+
+app = application
